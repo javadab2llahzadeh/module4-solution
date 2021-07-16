bd00eeebda8fe23fc7660768d0519654b8abb229
Javad <54034683+javadab2llahzadeh@users.noreply.github.com>
2021-07-15 18:38:05 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 (function () {
-var names = ["Ali", "Javad", "Mina", "Sara", "Iman", "Amir", "Mmd", "Asma", "Bita", "Nili"];
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
-
+  
   if (firstLetter === 'j') {
     byeSpeaker(names[i]);
   }
